d6f95440741e93a8a633034457727ea5bcc365c7
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-24 22:07:00 -0600

    Refactor titulos model to tarea with updated schema fields

--- models/titulos.js
+++ models/titulos.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const titulosSchema = new mongoose.Schema9({
-    id:{
-        type: Number,
-        required: true
-    },
-    }})
+const tareaSchema = new mongoose.Schema({
+    titulo:{type:String, required:true},
+    descripcion:{type:String, required:true},
+    estado:{type:Boolean, required:true}
+})
+const tarea = mongoose.model('tarea', tareaSchema);
+
+module.exports = {tarea};
