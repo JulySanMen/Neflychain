b0a705321082d12cad6e9460cf9a1ad9a234ac2f
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-24 22:28:33 -0600

    Add Titulo schema definition to titulos model

--- models/titulos.js
+++ models/titulos.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose');
-\
+
+const TituloSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    descripcion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    duracion: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    path: {
+        type: String,
+        required: true,
+        trim: true
+    },
+});
