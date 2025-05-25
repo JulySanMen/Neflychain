da77ebfc709d686a79214048eec941112f6339a9
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-24 22:20:20 -0600

    Remove tarea model and schema definition from titulos.js

--- models/titulos.js
+++ models/titulos.js
@@ -1,10 +1,2 @@
 const mongoose = require('mongoose');
-
-const tareaSchema = new mongoose.Schema({
-    titulo:{type:String, required:true},
-    descripcion:{type:String, required:true},
-    estado:{type:Boolean, required:true}
-})
-const tarea = mongoose.model('tarea', tareaSchema);
-
-module.exports = {tarea};
+\
