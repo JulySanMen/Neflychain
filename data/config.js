91aa0f8510c66a543a3c51d2805e3fb483469268
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-25 22:04:49 -0600

    Add database connection function to config.js

--- data/config.js
+++ data/config.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-const config ={
-    
+const connectDB = async()=>{
+    const url = 'mongodb://admin:password@localhost:27019/"';
+    try{
+        await mongoose.connect(url);
+        console.log('Conmectado a la Base de Datos <3');
+    }catch(err){
+        console.error('Error al conecatse a la Base de Datos </3')
+    }
 }
+module.export = {connectDB}
