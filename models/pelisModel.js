5b97bf8880c7790e9d3278cf4435e72d5994aacf
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-25 21:57:03 -0600

    Add pelisModel schema for peliculas data structure

---
+++ models/pelisModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const pelisModel = new mongoose.Schema({
+   titulo:{type:String, required:true},
+   descripcion:{type:String, required:true},
+   duracion:{type:String, required:true},
+   path:{type:String, required:true}
+});
+
+module.exports = {pelisModel};
