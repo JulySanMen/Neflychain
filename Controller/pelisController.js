29221c669481d205039865da4f40f1f1411b6a59
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-26 20:06:21 -0600

    Refactor pelisController to use class syntax and add getPelis method for retrieving peliculas

--- Controller/pelisController.js
+++ Controller/pelisController.js
@@ -1,10 +1,19 @@
 const { pelisService } = require('../service/pelisService');
 
-const pelisController = {
+class pelisController {
     async createPelis(req, res){
         const {titulo, descripcion, duracion, path} = req.body;
         const savePeli = await pelisService.createPelis(titulo, descripcion, duracion, path);
         res.json(savePeli)
+    }
+
+    async getPelis(req, res){
+        try{
+            const pelis = await pelisService.getPelis();
+            res.json(pelis);
+        }catch(err){
+            res.status(500).json({error: err.message});
+        }
     }
 }
 
