05fcc988185d6ea9684784518034bed728484bf5
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-26 20:06:25 -0600

    Add route for retrieving peliculas in pelisRoutes

--- routes/pelisRoutes.js
+++ routes/pelisRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const {pelisController} = require('../Controller/pelisController');
 
 router.post('/crear', pelisController.createPelis);
+router.get('/listar', pelisController.getPelis);
 
 module.exports = router
