2beeae0ba06283d43e6ee4aff476fcae9850a116
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-27 19:08:09 -0600

    Fix formatting by adding a trailing space in pelisRoutes.js

--- routes/pelisRoutes.js
+++ routes/pelisRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const {pelisController} = require('../Controller/pelisController');
 
-router.post('/crear', pelisController.createPelis);
+router.post('/crear', pelisController.createPelis); 
 router.get('/listar', pelisController.getPelis);
 
 module.exports = router
