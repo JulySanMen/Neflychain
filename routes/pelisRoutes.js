2779b86fe5645566454c39a10d84e7eeae1b8acc
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-05-28 19:42:09 -0600

    Fix pelisRoutes.js to export router as an object

--- routes/pelisRoutes.js
+++ routes/pelisRoutes.js
@@ -4,4 +4,4 @@ const {pelisController} = require('../Controller/pelisController');
 router.post('/crear', pelisController.createPelis); 
 router.get('/listar', pelisController.getPelis);
 
-module.exports = router
+module.exports = {router}
