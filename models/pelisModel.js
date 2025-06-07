a462f65cbc0492bebb6af53400b0aaf94e3d0b73
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-06 20:15:49 -0600

    Add image field to pelisSchema for enhanced movie data

--- models/pelisModel.js
+++ models/pelisModel.js
@@ -4,7 +4,8 @@ const pelisSchema = new mongoose.Schema({
    titulo:{type:String, required:true},
    descripcion:{type:String, required:true},
    duracion:{type:String, required:true},
-   path:{type:String, required:true}
+   path:{type:String, required:true},
+   image:{type: String, required:true}
 }); 
 const pelisModel = mongoose.model('pelis', pelisSchema);
 module.exports = {pelisModel};
