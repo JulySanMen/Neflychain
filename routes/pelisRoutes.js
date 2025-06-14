216df09f9e05f3da8a08da22e32876ba516a6f35
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-13 19:35:41 -0600

    Add authentication middleware to pelis routes for enhanced security

--- routes/pelisRoutes.js
+++ routes/pelisRoutes.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const {pelisController} = require('../Controller/pelisController')
 
-router.post('/crear', pelisController.createPelis); 
-router.get('/listar', pelisController.getPelis);
-router.post('/filtrar',pelisController.filterPelis);
+const router = require('express').Router();
+const {pelisController} = require('../Controller/pelisController');
+const { requireAuth } = require('../middleaware/authmiddleaware');
+
+router.post('/crear',requireAuth, pelisController.createPelis); 
+router.get('/listar',requireAuth, pelisController.getPelis);
+router.post('/filtrar',requireAuth, pelisController.filterPelis);
 
 module.exports = router
