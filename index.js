f192e6fb136bae333f6597bbad8ed11109d79010
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-13 19:36:13 -0600

    Enhance CORS configuration and integrate passport for authentication

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const pelisRoutes = require('./routes/pelisRoutes');
 const cors = require('cors');
 const app = express();
 const connectDB = require('./data/config');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const configurePassport = require('./config/passport'); // ajusta la ruta si es diferente
 
 const PORT = 3000;
-app.use(cors());
+app.use(cors({credentials:true}));
 app.use(express.json());
+
+app.use(cookieParser());
+configurePassport(passport);
+app.use(passport.initialize());
+
+
 app.use('/pelis', pelisRoutes);
 connectDB()
 
